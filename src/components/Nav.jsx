d31d552e914fa74ca75c6cdeb0708779e5f9e21a
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-24 16:07:52 +0900

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import useSmoothScroll from '../hooks/useSmoothScroll'
-const Nav = () => {
-  const navLink = ['Hero', 'Brand', 'Products', 'Ingredients', 'Reviews', 'Cta']
 
-  const scrollTo = useSmoothScroll()
+const Nav = () => {
+
   return (
     <nav>
-      <ul>
-        {navLink.map((nav, i) => (
-
-          <li key={i}>
-            <a href={`#${nav}`}
-              onClick={(e) => {
-                e.preventDefault()
-                scrollTo(nav)
-              }} >
-              {nav}
-            </a>
-          </li>
-        ))}
-      </ul>
-
+      nav
     </nav>
   )
 }
