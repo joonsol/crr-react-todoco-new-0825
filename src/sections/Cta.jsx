bc08e5bab0fb769e1d60e5f309d7aa41fc53f804
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-28 15:53:42 +0900

    .

--- src/sections/Cta.jsx
+++ src/sections/Cta.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import {categories} from '../util/cate'
+import { categories } from '../util/cate'
 import '../styles/sections/_cta.scss'
 const Cta = () => {
   return (
     <div className='inner'>
       <ul className='cta-lst'>
-        {categories.map((cta)=>(
+        {categories.map((cta) => (
           <li key={cta.id}>
             <a href={cta.href}>
-<div className="img-wrap" style={{backgroundImage:`url(${cta.img.src})`}}>
+              <div className="img-wrap" style={{ backgroundImage: `url(${cta.img.src})` }}>
 
-     
-</div>
-            <p>
-              {cta.name}
-            </p>
+
+              </div>
+              <p>
+                {cta.name}
+              </p>
             </a>
           </li>
         ))}
--- src/sections/Hello.jsx
+++ src/sections/Hello.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import {helloData} from '../util/hello'
 const Hello = () => {
   return (
-    <div>Hello</div>
+
+    <div className='inner hello-inner'>
+      <div className="img-wrap">
+        <img src={helloData.media.src} alt={helloData.media.alt} />
+      </div>
+      <div className="t-wrap">
+        <p className="txt-2">
+          {helloData.eyebrow}
+        </p>
+        <h1 className="tit">
+          {helloData.title}
+        </h1>
+      </div>
+    </div>
   )
 }
 
