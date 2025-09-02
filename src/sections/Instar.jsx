fe124e6039783cfd60433f15228df43e8d69c2b6
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-02 15:14:11 +0900

    width: ;

--- src/sections/Instar.jsx
+++ src/sections/Instar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import instar from "../util/instar"
+import "../styles/sections/_instar.scss"
 const Instar = () => {
   return (
-    <div>Instar</div>
+    <div className='inner instar-inner'
+    >
+      <div className="t-wrap">
+        <h2 className="con-tit">
+          Instargram        </h2>
+        <p className="txt-4">
+          @tocobo_official
+        </p>
+      </div>
+      <ul className="instar-lst">
+        {instar.map((i)=>(
+          <li key={i.id}>
+            <a href={i.link} style={{backgroundImage:`url(${i.image})`}} alt={i.alt}>{i.alt}</a>
+          </li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
--- src/sections/SkinCare.jsx
+++ src/sections/SkinCare.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
-
+import skincare from "../util/skincare"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import "../styles/sections/_skincare.scss"
+import 'swiper/css'
 const SkinCare = () => {
   return (
-    <div>SkinCare</div>
+    <div className='inner skincare-inner'>
+      <div className="tit_wrap">
+        <h2 className="con-tit">
+          Vegan  <br className="m_only" />
+          Skincare
+        </h2>
+        <p className="txt-3">피부에 건강한 영향력을 전달하고 꼭 필요한 성분만을 담아 <br />
+          놀라운 변화를 선사하는 비건 스킨케어 브랜드 토코보
+        </p>
+        <a href="#" className="con_btn">
+          Meet Tocobo
+        </a>
+      </div>
+
+      <div className="skincare-sl-wrap">
+        <div className="in">
+
+          <Swiper
+            modules={[Autoplay]}
+            className="swiper hero_slider"
+            slidesPerView={2}
+            loop={true}
+                 spaceBetween={30}
+          // autoplay={{ delay: 5000 }}
+          >
+            {skincare.map((slide) => (
+              <SwiperSlide
+                key={slide.id}
+                className={`hero-slide ${slide.id}`}
+
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              >
+                <a href={slide.link}>
+                  {slide.id}
+                </a>
+              </SwiperSlide>
+            ))}
+
+
+          </Swiper>
+
+
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 
